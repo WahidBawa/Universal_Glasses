59dc542682bb53cce0aec5f343bb2714f1c1a8bc
Wahid Bawa <22acde1c1b75c4614b41b626ea2ab8db93744cdb>
2018-11-27 07:38:17 -0500

    Video Edit Theory Proven

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    _, frame = cap.read()
 
     # Our operations on the frame come here
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -14,9 +14,15 @@ while(True):
 
     # Display the resulting frame
     # cv2.imshow('grayframe',gray)
+    # print(frame[0, 0])z
+    for f in range(100):
+        for i in range(100):
+            for n in range(3):
+                frame[f, i][n] = 0 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+#640 by 480
