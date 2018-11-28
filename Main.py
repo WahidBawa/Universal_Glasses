e4f3294269369af92e1da2347906d750be82bb16
Wahid Bawa <22acde1c1b75c4614b41b626ea2ab8db93744cdb>
2018-11-28 10:39:27 -0500

    Stuff has been done

--- Main.py
+++ Main.py
@@ -4,27 +4,47 @@ import cv2
 import colorsys
 
 cap = cv2.VideoCapture(0)
+def normalise(editablePhoto,sizeX,sizeY):
+    NormalPhoto =  np.zeros((sizeX,sizeY,3),'float')
+    x=sizeX-1
+    y=sizeY
+    for i in range(0,sizeX):
+        for j in range(0,sizeY):
+            for k in range(0,3):
+                NormalPhoto[x,j,k]=editablePhoto[i,j,k]
+        x=x-1
+
+    return NormalPhoto
+
+def arrayToImage(editablePhoto,sizeX,sizeY,saveAs):
+    rgbArray = np.zeros((sizeX,sizeY,3),'uint8')
+    for i in range(0,sizeX):
+        for j in range(0,sizeY):
+            for k in range(0,3):
+                rgbArray[i,j,k] = editablePhoto[i,j,k]
+    return rgbArray
+    # img = Image.fromarray(rgbArray)
+    # img.save(saveAs)
 
 while(True):
     # Capture frame-by-frame
     _, frame = cap.read()
-
+    width, height = 100, 100
     # Our operations on the frame come here
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    height, width = frame.shape[:2]
+    # height, width = frame.shape[:2]
     # frame = cv2.resize(frame,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
     # Display the resulting frame
     # cv2.imshow('grayframe',gray)
     # print(frame[0, 0])
 
 
-
     editablePhoto = np.zeros((width,height,3),'float')
     hsvArray=np.zeros((width,height,3),'float')
                 
-    for i in range(0,width):
-        for j in range(0,height):
+    for i in range(0,25):
+        for j in range(0,25):
             for k in range(0,3):
                 editablePhoto[i,j,k] = frame[i,j][k]
                 editablePhoto[i,j,k] = ((editablePhoto[i,j,k])/255)
@@ -51,9 +71,9 @@ while(True):
             for k in range(0,3):
                 editablePhoto[i,j, k] = tempArray[k]*255
 
+            NormalPhoto = normalise(editablePhoto, width, height)
 
-
-    cv2.imshow('frame',editablePhoto)
+    cv2.imshow('frame', )
     # cv2.imshow('a',res)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
