ee27640c88c9629d4913d2748fd99bbac0301558
Wahid Bawa <22acde1c1b75c4614b41b626ea2ab8db93744cdb>
2018-11-28 11:06:17 -0500

    Remote Camera Access Granted

--- Main.py
+++ Main.py
@@ -3,28 +3,29 @@ import numpy as np
 import cv2
 import colorsys
 
-cap = cv2.VideoCapture(0)
-def normalise(editablePhoto,sizeX,sizeY):
-    NormalPhoto =  np.zeros((sizeX,sizeY,3),'float')
-    x=sizeX-1
-    y=sizeY
-    for i in range(0,sizeX):
-        for j in range(0,sizeY):
-            for k in range(0,3):
-                NormalPhoto[x,j,k]=editablePhoto[i,j,k]
-        x=x-1
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("http://10.88.206.32:4747/mjpegfeed?640x480")
+# def normalise(editablePhoto,sizeX,sizeY):
+#     NormalPhoto =  np.zeros((sizeX,sizeY,3),'float')
+#     x=sizeX-1
+#     y=sizeY
+#     for i in range(0,sizeX):
+#         for j in range(0,sizeY):
+#             for k in range(0,3):
+#                 NormalPhoto[x,j,k]=editablePhoto[i,j,k]
+#         x=x-1
 
-    return NormalPhoto
+#     return NormalPhoto
 
-def arrayToImage(editablePhoto,sizeX,sizeY,saveAs):
-    rgbArray = np.zeros((sizeX,sizeY,3),'uint8')
-    for i in range(0,sizeX):
-        for j in range(0,sizeY):
-            for k in range(0,3):
-                rgbArray[i,j,k] = editablePhoto[i,j,k]
-    return rgbArray
-    # img = Image.fromarray(rgbArray)
-    # img.save(saveAs)
+# def arrayToImage(editablePhoto,sizeX,sizeY,saveAs):
+#     rgbArray = np.zeros((sizeX,sizeY,3),'uint8')
+#     for i in range(0,sizeX):
+#         for j in range(0,sizeY):
+#             for k in range(0,3):
+#                 rgbArray[i,j,k] = editablePhoto[i,j,k]
+#     return rgbArray
+#     # img = Image.fromarray(rgbArray)
+#     # img.save(saveAs)
 
 while(True):
     # Capture frame-by-frame
@@ -32,7 +33,7 @@ while(True):
     width, height = 100, 100
     # Our operations on the frame come here
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # height, width = frame.shape[:2]
     # frame = cv2.resize(frame,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
     # Display the resulting frame
@@ -40,40 +41,49 @@ while(True):
     # print(frame[0, 0])
 
 
-    editablePhoto = np.zeros((width,height,3),'float')
-    hsvArray=np.zeros((width,height,3),'float')
+    # editablePhoto = np.zeros((width,height,3),'float')
+    # hsvArray=np.zeros((width,height,3),'float')
                 
-    for i in range(0,25):
-        for j in range(0,25):
-            for k in range(0,3):
-                editablePhoto[i,j,k] = frame[i,j][k]
-                editablePhoto[i,j,k] = ((editablePhoto[i,j,k])/255)
-            rNew=editablePhoto[i,j,0]
-            gNew=editablePhoto[i,j,1]
-            bNew=editablePhoto[i,j,2]
+    # for i in range(0,25):
+    #     for j in range(0,25):
+    #         for k in range(0,3):
+    #             editablePhoto[i,j,k] = frame[i,j][k]
+    #             editablePhoto[i,j,k] = ((editablePhoto[i,j,k])/255)
+    #         rNew=editablePhoto[i,j,0]
+    #         gNew=editablePhoto[i,j,1]
+    #         bNew=editablePhoto[i,j,2]
 
-            tempArray=np.zeros((3),'float')
+    #         tempArray=np.zeros((3),'float')
 
-            for k in range(0,3):
-                hsvArray[i,j,k]=colorsys.rgb_to_hsv(editablePhoto[i,j,0],editablePhoto[i,j,1],editablePhoto[i,j,2])[k]
+    #         for k in range(0,3):
+    #             hsvArray[i,j,k]=colorsys.rgb_to_hsv(editablePhoto[i,j,0],editablePhoto[i,j,1],editablePhoto[i,j,2])[k]
 
-            print(gNew)
-            greenRatio = 0 if gNew == 0 else (hsvArray[i,j,0] - (60/360))/gNew
-            blueRange = greenRatio*bNew
-            hsvArray[i,j,0] = 0.5 + blueRange
+    #         print(gNew)
+    #         greenRatio = 0 if gNew == 0 else (hsvArray[i,j,0] - (60/360))/gNew
+    #         blueRange = greenRatio*bNew
+    #         hsvArray[i,j,0] = 0.5 + blueRange
 
-            tempArray=np.zeros((3),'float')
-            for k in range(0,3):
-                tempArray[k]=hsvArray[i,j,k]
-            tempArray.tolist()
-            tempArray = (colorsys.hsv_to_rgb(tempArray[0],tempArray[1],tempArray[2]))
+    #         tempArray=np.zeros((3),'float')
+    #         for k in range(0,3):
+    #             tempArray[k]=hsvArray[i,j,k]
+    #         tempArray.tolist()
+    #         tempArray = (colorsys.hsv_to_rgb(tempArray[0],tempArray[1],tempArray[2]))
 
-            for k in range(0,3):
-                editablePhoto[i,j, k] = tempArray[k]*255
+    #         for k in range(0,3):
+    #             editablePhoto[i,j, k] = tempArray[k]*255
 
-            NormalPhoto = normalise(editablePhoto, width, height)
-
-    cv2.imshow('frame', )
+    #         NormalPhoto = normalise(editablePhoto, width, height)
+    for i in range(0, 100):
+        for j in range(0, 100):
+            for k in range(0, 3):
+                if k == 0:
+                    frame[i, j][k] *= 1.15
+                if k == 1:
+                    frame[i, j][k] *= 2.4546
+                if k == 2:
+                    frame[i, j][k] *= .56465
+                        
+    cv2.imshow('frame', frame)
     # cv2.imshow('a',res)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
