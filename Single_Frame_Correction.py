f8007ec7e4ff9df1bb105a4494f86e701bf0b073
Wahid Bawa <22acde1c1b75c4614b41b626ea2ab8db93744cdb>
2018-12-04 02:53:20 -0500

    Presentation Ready

--- Single_Frame_Correction.py
+++ Single_Frame_Correction.py
@@ -3,14 +3,13 @@ import numpy as np
 import cv2
 import colorsys
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture("http://10.88.206.32:8080/video")
 # cap = cv2.VideoCapture("http://10.88.206.32:4747/mjpegfeed?640x480")
 
-#MOTO G3
-# cap = cv2.VideoCapture("http://192.168.0.75:8080/video")
 # cap = cv2.VideoCapture("http://192.168.0.75:4747/mjpegfeed?640x480")
-cap = cv2.VideoCapture("http://10.42.0.248:4747/mjpegfeed?640x480")
+cap = cv2.VideoCapture("http://192.168.0.75:4747/mjpegfeed?320x240")
+# cap = cv2.VideoCapture("http://10.42.0.248:4747/mjpegfeed?640x480")
 # cap = cv2.VideoCapture("http://10.42.0.248:4747/mjpegfeed?320x240")
 
 while(True):
@@ -19,8 +18,9 @@ while(True):
 
     cv2.imshow('img1',frame) #display the captured image
     if cv2.waitKey(1) & 0xFF == ord('q'): #save on pressing 'q' 
-        # height, width = 320, 240
-        height, width = 640, 480
+        cv2.imwrite('normal.png', frame)
+        height, width = 320, 240
+        # height, width = 640, 480
         editablePhoto = np.zeros((width,height,3),'float')
         hsvArray=np.zeros((width,height,3),'float')
                     
@@ -52,14 +52,13 @@ while(True):
                 for k in range(0,3):
                     editablePhoto[i,j, k] = tempArray[k]*255
                 frame[i, j, k] = editablePhoto[i, j, k]
-        # NormalPhoto = normalise(editablePhoto, 100, 100)
         NormalPhoto = editablePhoto
         for i in range(0,width):
             for j in range(0,height):
                 for k in range(0,3):
                     frame[i,j,k] = NormalPhoto[i, j, k]
         # arrayToImage(NormalPhoto,25,25,saveAs)
-        cv2.imwrite('c1.png',frame)
+        cv2.imwrite('correct.png',frame)
         cv2.destroyAllWindows()
         break
 
